6ffbf56bf403e3df23edf5c15daa45019f78b209
smml1996 <michelle_muroya@hotmail.com>
2022-06-28 12:34:59 +0200

    main workflow

--- main.py
+++ main.py
@@ -2,11 +2,29 @@ import sys
 from utils import *
 from qasm_gen import *
 from instructions import Instruction
+from settings import *
 
 n = int(sys.argv[1])
 input_file = sys.argv[2]
 output_file = sys.argv[3]
 
+current_settings = get_btor2_settings(input_file)
+Instruction.all_instructions = read_file(input_file, modify_memory_sort=True, setting=current_settings)
+
 for i in range(n):
+
     # clear nids set of nids created in the previous timestep
     Instruction.created_nids_in_timestep.clear()
+
+    for instruction in Instruction.all_instructions.values():
+        if instruction[1] == INIT and i == 1:
+            Instruction(instruction).execute()
+        elif instruction[1] == NEXT:
+            Instruction(instruction).execute()
+        elif instruction[1] == BAD:
+            Instruction(instruction).execute()
+
+    # TODO: GARBAGE COlLECTOR algorithm
+
+# TODO: logic-OR bad states
+Instruction.circuit.qasm(filename=output_file)
--- utils.py
+++ utils.py
@@ -76,3 +76,79 @@ def bit_level_sum(bits1: List[int], bits2: List[int]) -> List[int]:
     if carry == 1:
         print(f"WARNING {carry}: overflow at tools.bit_level_sum")
     return result
+
+def get_btor2_settings(filename: str) -> Dict[str, int]:
+    WORD_SIZE = 64
+    file = open(filename, 'r')
+    result = {"begin_datasegment": 0, "begin_heap": 0, "size_datasegment": 0, "size_heap": 0, 'size_stack':0}
+
+    is_4_bit_address_space = False
+    is_29_bit_address_space = False
+    is_constant_propagation = False
+    lines = file.readlines()
+
+    for i in range(len(lines)):
+        line = lines[i]
+        if "./beator-32" in line:
+            WORD_SIZE = 32
+        elif "with --constant-propagation" in line:
+            is_constant_propagation = True
+        elif  "; with --MMU" in line:
+            is_4_bit_address_space = True
+        elif "; with --RAM" in line:
+            is_4_bit_address_space = True
+        elif "; with --linear-address-space" in line:
+            is_29_bit_address_space = True
+        elif "total memory" in line and "data" in line and "heap" in line and "stack" in line:
+            elements = line.split(",")
+            #assert len(elements) == 4
+            for element in elements:
+                if 'data' in element:
+                    more_elements = element.split('B')
+                    result["size_datasegment"] = int(int(more_elements[0]) / (WORD_SIZE/8))
+                elif 'heap' in element:
+                    more_elements = element.split('B')
+                    result['size_heap'] = int(int(more_elements[0]) / (WORD_SIZE/8))
+                elif 'stack' in element:
+                    more_elements = element.split('B')
+                    result["size_stack"] = int(int(more_elements[0]) / (WORD_SIZE/8))
+        elif not is_4_bit_address_space:
+            if is_29_bit_address_space:
+                if WORD_SIZE == 64:
+                    n = 29
+                else:
+                    n = 30
+            else:
+                n = 64
+            if f"start of data segment in {n}-bit" in line or (WORD_SIZE == 32 and f"start of data segment in 32-bit" in line):
+                next_line = lines[i+1].split()
+                result['begin_datasegment'] = int(next_line[3])
+            elif f"start of heap segment in {n}-bit" in line or (WORD_SIZE == 32 and f"start of heap segment in 32-bit" in line):
+                next_line = lines[i+1].split()
+                result['begin_heap'] = int(next_line[3])
+    file.close()
+    result["word_size"] = WORD_SIZE
+    if is_4_bit_address_space:
+        result["address_word_size"] = 4
+        result["address_step_size"] = 1
+        result["begin_datasegment"] = 0
+        result["begin_heap"] = result["size_datasegment"]
+        result["begin_stack"] = result["size_datasegment"] + result["size_heap"] + result["size_stack"]
+    elif is_29_bit_address_space:
+
+        result["address_step_size"] = 1
+        if WORD_SIZE == 64:
+            result["address_word_size"] = 29
+            result["begin_stack"] = 4294967296 >> 3
+        else:
+            result["address_word_size"] = 30
+            result["begin_stack"] = 4294967296 >> 2
+    else:
+        # if is_constant_propagation:
+        #     result["address_word_size"] = 32
+        # else:
+        result["address_word_size"] = WORD_SIZE
+        result["address_step_size"] = int(WORD_SIZE/8)
+        result["begin_stack"] = 4294967296
+    # assert len(result.keys()) == 9
+    return result
